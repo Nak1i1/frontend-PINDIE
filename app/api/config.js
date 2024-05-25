812cedcabe877dd57cf2a63884ae96703e27902d
Nak1i1 <147645927+Nak1i1@users.noreply.github.com>
2024-05-26 00:56:39 +0300

    ффф

--- app/api/config.js
+++ app/api/config.js
@@ -1,7 +1,9 @@
 export const BASE_URL = 'https://backend-connec.nomoredomainswork.ru/api'
 
 export const endpoints = {
-  games: `${BASE_URL}/games`,
-  auth: `${BASE_URL}/auth/login`,
-  me: `${BASE_URL}/me`,
-};
+	games: `${BASE_URL}/games`,
+	auth: `${BASE_URL}/auth/login`,
+	me: `${BASE_URL}/me`,
+}
+
+
